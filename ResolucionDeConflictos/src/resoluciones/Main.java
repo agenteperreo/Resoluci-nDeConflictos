ec166adae8bcacf00fef1035aafb5d5ca4069d53
igarcia <igarcar@iesnervion.es>
2022-12-15 12:16:18 +0100

    15/12/2022

--- ResolucionDeConflictos/src/resoluciones/Main.java
+++ ResolucionDeConflictos/src/resoluciones/Main.java
@@ -3,10 +3,7 @@ package resoluciones;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola, soy Isaac García");
-		System.out.println("èhe");
-
+		
 	}
 
 }
